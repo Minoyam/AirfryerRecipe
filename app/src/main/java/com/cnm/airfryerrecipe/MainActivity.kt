626d17a78b0e74c7726fd35ef3d51248f3d9e7dd
mino <k3000js@naver.com>
2020-03-26 19:04:51 +0900

    카테고리 리사이클러 연결

--- app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
+++ app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.cnm.airfryerrecipe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val categoryAdapter = CategoryAdapter()
+    private val item = listOf<CategoryResponse>(CategoryResponse("치킨", R.mipmap.ic_launcher_round))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_content.apply {
+            adapter = categoryAdapter
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
+        }
+        categoryAdapter.setItem(item)
     }
 }
