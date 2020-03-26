81e9ee56249b4ff138f7a155909605ef669255a1
mino <k3000js@naver.com>
2020-03-26 19:39:04 +0900

    Main에 binding 적용

--- app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
+++ app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
@@ -2,17 +2,23 @@ package com.cnm.airfryerrecipe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.cnm.airfryerrecipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val categoryAdapter = CategoryAdapter()
-    private val item = listOf<CategoryResponse>(CategoryResponse("치킨", R.mipmap.ic_launcher_round))
+    private val item =
+        listOf(CategoryResponse("치킨", R.drawable.ic_launcher_background))
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rv_content.apply {
+        binding.rvContent.apply {
             adapter = categoryAdapter
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
