168a5e47507c19cf06657556d43e7ad45edb0826
mino <k3000js@naver.com>
2020-03-31 19:32:21 +0900

    Response 수정

--- app/src/main/java/com/cnm/airfryerrecipe/CategoryResponse.kt
+++ app/src/main/java/com/cnm/airfryerrecipe/CategoryResponse.kt
@@ -1,6 +1,10 @@
 package com.cnm.airfryerrecipe
 
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class CategoryResponse(
-    val categoryName : String,
-    val categoryImage : Int
+    val categoryName: String? = "",
+    val categoryImage: String? = ""
 )
