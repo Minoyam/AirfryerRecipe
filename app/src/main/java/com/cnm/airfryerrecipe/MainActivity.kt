ba381e337132faed5cf81cf9c590e6249ca81321
mino <k3000js@naver.com>
2020-03-31 20:07:49 +0900

    Category firebase 연결

--- app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
+++ app/src/main/java/com/cnm/airfryerrecipe/MainActivity.kt
@@ -5,23 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cnm.airfryerrecipe.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
     private val categoryAdapter = CategoryAdapter()
-    private val item =
-        listOf(CategoryResponse("치킨", R.drawable.ic_launcher_background))
     private val binding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
-
+    lateinit var firebaseDatabase: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference("category")
         binding.rvContent.apply {
             adapter = categoryAdapter
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
-        categoryAdapter.setItem(item)
+
+        firebaseDatabase.addValueEventListener(object : ValueEventListener {
+            val item = mutableListOf<CategoryResponse?>()
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                item.clear()
+                for (postSnapshot in dataSnapshot.children) {
+                    item.add(postSnapshot.getValue(CategoryResponse::class.java))
+                }
+                categoryAdapter.setItem(item)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                // ...
+            }
+        })
+
     }
+
 }
