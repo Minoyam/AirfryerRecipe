0072b16feb8c8364ef5ff1582187a841201396cf
mino <k3000js@naver.com>
2020-04-06 02:16:31 +0900

    bindSetCategoryItem 추가

---
+++ app/src/main/java/com/cnm/airfryerrecipe/adapter/BindingAdapter.kt
@@ -0,0 +1,22 @@
+package com.cnm.airfryerrecipe.adapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.cnm.airfryerrecipe.data.model.CategoryResponse
+
+@BindingAdapter("bind:bindImage")
+fun bindImage(imageView: ImageView, imageUri: String?) {
+    imageUri?.let {
+        Glide.with(imageView.context).load(imageUri).override(180,180).into(imageView)
+    }
+
+}
+@BindingAdapter("bind:bindSetMovieItem")
+fun RecyclerView.bindSetCategoryItem(items: List<CategoryResponse?>) {
+    if (adapter is CategoryAdapter)
+        items.let {
+            (adapter as CategoryAdapter).setItem(it)
+        }
+}
