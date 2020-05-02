3bfdb7559f9c8b2fda2d5e5764ced8d550a08f41
mino <k3000js@naver.com>
2020-05-02 20:42:22 +0900

    CategoryResponse 수정
    categoryID 추가

--- app/src/main/java/com/cnm/airfryerrecipe/data/model/CategoryResponse.kt
+++ app/src/main/java/com/cnm/airfryerrecipe/data/model/CategoryResponse.kt
@@ -1,10 +1,10 @@
 package com.cnm.airfryerrecipe.data.model
 
-import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class CategoryResponse(
     val categoryName: String? = "",
-    val categoryImage: String? = ""
+    val categoryImage: String? = "",
+    val categoryId: String? = ""
 )
